cd2c9d4755216c1aa9ab07f7c26426893b8814cc
jumin <5216863@gmail.com>
2024-02-01 14:46:59 +0900

    add cors setting

---
+++ src/main/java/com/enm/costcalculrator/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.enm.costcalculrator.config;
+
+import org.springframework.context.annotation.Configuration;
+        import org.springframework.web.servlet.config.annotation.CorsRegistry;
+        import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // CORS 설정
+        registry.addMapping("/**")
+                .allowedOrigins("https://localhost:3000")
+                .allowedOrigins("http://localhost:3000");
+    }
+}
