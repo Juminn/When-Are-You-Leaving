6efc4a76baab4ae82b16ea922b6ddee68994f9d3
jumin <5216863@gmail.com>
2024-06-23 15:47:39 +0900

    refactory/naverMapAPI edit Dockerfile and rateTime 25/s and 1200s timeout

--- src/main/java/com/enm/costcalculrator/config/WebConfig.java
+++ src/main/java/com/enm/costcalculrator/config/WebConfig.java
@@ -40,7 +40,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public RateLimiter rateLimiter() {
         RateLimiterConfig config = RateLimiterConfig.custom()
-                .limitForPeriod(50) // 초당 최대 요청 수
+                .limitForPeriod(25) // 초당 최대 요청 수
                 .limitRefreshPeriod(Duration.ofSeconds(1)) // 리프레시 주기
                 .timeoutDuration(Duration.ofSeconds(1200)) // 요청 타임아웃 기간
                 .build();
