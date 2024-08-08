9466ddf3260d83988833f5cc7d612430a1c6459f
jumin <5216863@gmail.com>
2024-08-08 14:28:00 +0900

    test/MapAPI rateLimit 1/Min

--- src/main/java/com/enm/costcalculrator/config/WebConfig.java
+++ src/main/java/com/enm/costcalculrator/config/WebConfig.java
@@ -40,8 +40,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public RateLimiter rateLimiter() {
         RateLimiterConfig config = RateLimiterConfig.custom()
-                .limitForPeriod(10) // 분당 최대 요청 수
-                .limitRefreshPeriod(Duration.ofSeconds(60)) // 리프레시 주기
+                .limitForPeriod(1) //
+                .limitRefreshPeriod(Duration.ofSeconds(60)) // 1회 요청주기 10초
                 .timeoutDuration(Duration.ofSeconds(1200)) // 요청 타임아웃 기간
                 .build();
 
